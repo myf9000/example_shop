ffb344ad4b6924548be2cdac1c586040306348da
myf900 <myf9000@vp.pl>
2015-11-10 10:31:53 +0100

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/about', as: :about
+
+  get 'static_pages/faq', as: :faq
+
+  get 'static_pages/news', as: :news
+
   resources :products
 
   root 'products#index'
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+  test "should get news" do
+    get :news
+    assert_response :success
+  end
+
+end
